e68056bc7e2f702a05fb5957519968a3fc00101c
se7t <kacper.kochan@protonmail.com>
2023-02-03 17:04:05 +0100

    LMB-2 Create homepage (#2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Changa One", "sans-serif"],
+    },
   },
   plugins: [],
 };
