b578a54d635f47ad48c2036ee6f39f9f158e71c5
se7t <kacper.kochan@protonmail.com>
2023-02-05 18:53:48 +0100

    LMB-4 Implement displaying of all minifig parts

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -3,10 +3,35 @@ import { useContext, useEffect, useState } from "react";
 import { MinifigContext } from "@/lib/MinifigContext";
 import axios from "axios";
 
+interface MinifigPart {
+  part: {
+    part_img_url: string;
+    name: string;
+    part_num: string;
+  };
+}
+
+interface ChosenMinifigParts {
+  results: MinifigPart[];
+  count: number;
+}
+
 export const Summary = () => {
   const { chosenMinifigId } = useContext(MinifigContext);
 
-  const [chosenMinifigParts, setChosenMinifigParts] = useState({});
+  const [chosenMinifigParts, setChosenMinifigParts] =
+    useState<ChosenMinifigParts>({
+      results: [
+        {
+          part: {
+            part_img_url: "",
+            name: "",
+            part_num: "",
+          },
+        },
+      ],
+      count: 0,
+    });
 
   const rebrickableEndpoint = `https://rebrickable.com/api/v3/lego/minifigs/${chosenMinifigId}/parts?key=${process.env.NEXT_PUBLIC_REBRICKABLE_API_KEY}`;
 
@@ -23,7 +48,7 @@ export const Summary = () => {
   }, [rebrickableEndpoint]);
 
   return (
-    <div className="flex flex-shrink flex-grow-0 basis-96 flex-col gap-8 rounded-xl bg-white p-8">
+    <div className="flex flex-shrink flex-grow-0 basis-96 flex-col  gap-8 rounded-xl bg-white p-8">
       <h2 className="text-bold font-display text-3xl uppercase text-black sm:text-4xl">
         Summary
       </h2>
@@ -40,20 +65,28 @@ export const Summary = () => {
         Harry Potter, Open Dark Blue Jacket over White Shirt, Dark Bluish Gray
         Legs
       </h3>
-      <p className="font-bold">There are {4} parts in this minifig:</p>
-      <div className="flex gap-2">
-        <Image
-          src="https://cdn.rebrickable.com/media/parts/elements/4157659.jpg"
-          width={64}
-          height={64}
-          alt={"Hair Short Tousled"}
-          className="h-auto w-16"
-        />
-        <div>
-          <p className="font-bold">Hair Short Tousled</p>
-          <p className="font-bold text-amber-400">40233</p>
-        </div>
-      </div>
+      <p className="font-bold">
+        There are {chosenMinifigParts.count} parts in this minifig:
+      </p>
+      {chosenMinifigParts.results && chosenMinifigParts.results.length > 0
+        ? chosenMinifigParts.results.map((minifigPart, index) => (
+            <div key={index} className="flex items-start gap-2">
+              <Image
+                src={minifigPart.part.part_img_url}
+                width={64}
+                height={64}
+                alt={minifigPart.part.name}
+                className="h-auto w-16"
+              />
+              <div>
+                <p className="font-bold">{minifigPart.part.name}</p>
+                <p className="font-bold text-amber-400">
+                  {minifigPart.part.part_num}
+                </p>
+              </div>
+            </div>
+          ))
+        : null}
     </div>
   );
 };
