bd114796ba4f8cea88ff723d8fe608bf0a7b2f0b
se7t <kacper.kochan@protonmail.com>
2023-02-06 00:21:50 +0100

    LMB-19: Add View Source link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import "@fontsource/changa-one";
 import "@fontsource/exo-2/variable.css";
 import { MinifigProvider } from "@/lib/MinifigContext";
+import { ViewSource } from "@/components/ViewSource";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MinifigProvider>
       <Component {...pageProps} />
+      <ViewSource />
     </MinifigProvider>
   );
 }
