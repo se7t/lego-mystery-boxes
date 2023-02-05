5669301a68310442f29adbc230bbc2d15bcfcb18
se7t <kacper.kochan@protonmail.com>
2023-02-05 19:11:26 +0100

    LMB-4 Add Head to shipment page

--- src/pages/shipment.tsx
+++ src/pages/shipment.tsx
@@ -2,7 +2,7 @@ import Form from "@/components/Form";
 import { Summary } from "@/components/Summary";
 import { MinifigContext } from "@/lib/MinifigContext";
 import { useContext } from "react";
-import Link from "next/link";
+import Head from "next/head";
 import { MinifigsNotLoaded } from "@/components/MinifigsNotLoaded";
 
 export default function Home() {
@@ -10,6 +10,15 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>Free Minifig Shipment Details | se7t.dev</title>
+        <meta
+          name="description"
+          content="Grab your free LEGO Minifig from the Harry Potter Universe."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="container mx-auto flex  items-center justify-center gap-8 px-8">
         {chosenMinifigId.length === 0 ? (
           <MinifigsNotLoaded />
