75e90030647e73d1d2a4038b752b5d4abdd69e50
se7t <kacper.kochan@protonmail.com>
2023-02-05 15:52:37 +0100

    LMB-4 Move Summary to standalone component

--- src/pages/shipment.tsx
+++ src/pages/shipment.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Summary } from "@/components/Summary";
 
 export default function Home() {
   return (
@@ -10,40 +10,7 @@ export default function Home() {
               Order
             </h1>
           </div>
-          <div className="flex flex-shrink flex-grow-0 basis-96 flex-col gap-8 rounded-xl bg-white p-8">
-            <h2 className="text-bold font-display text-3xl uppercase text-black sm:text-4xl">
-              Summary
-            </h2>
-            <Image
-              src={
-                "https://cdn.rebrickable.com/media/sets/fig-000029/60572.jpg"
-              }
-              width={200}
-              height={200}
-              alt={
-                "Harry Potter, Open Dark Blue Jacket over White Shirt, Dark Bluish Gray Legs"
-              }
-              className="self-center"
-            />
-            <h3 className="text-center font-bold">
-              Harry Potter, Open Dark Blue Jacket over White Shirt, Dark Bluish
-              Gray Legs
-            </h3>
-            <p className="font-bold">There are {4} parts in this minifig:</p>
-            <div className="flex gap-2">
-              <Image
-                src="https://cdn.rebrickable.com/media/parts/elements/4157659.jpg"
-                width={64}
-                height={64}
-                alt={"Hair Short Tousled"}
-                className="h-auto w-16"
-              />
-              <div>
-                <p className="font-bold">Hair Short Tousled</p>
-                <p className="font-bold text-amber-400">40233</p>
-              </div>
-            </div>
-          </div>
+          <Summary />
         </div>
       </div>
     </>
