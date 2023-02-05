ec3fc3123cec22da7b95cdfae5c6e33c25a1114d
se7t <kacper.kochan@protonmail.com>
2023-02-05 14:11:11 +0100

    LMB-3 Refactor Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,10 +7,10 @@ interface ButtonProps {
   onClickAction: () => void;
 }
 
-function Button({ children, href, onClickAction }: ButtonProps) {
+const Button = ({ children, href, onClickAction }: ButtonProps) => {
   const router = useRouter();
 
-  const handleClick = async (e: MouseEvent<HTMLAnchorElement>) => {
+  const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     onClickAction();
     router.push(href);
@@ -20,11 +20,11 @@ function Button({ children, href, onClickAction }: ButtonProps) {
     <a
       href={href}
       onClick={handleClick}
-      className="active:px-25 font-sans rounded-full bg-blue-500 px-24 py-3 text-lg font-bold uppercase text-white drop-shadow-lg transition-all hover:bg-blue-600 focus:bg-blue-700 active:bg-blue-700"
+      className="px-25 font-sans rounded-full bg-blue-500 px-24 py-3 text-lg font-bold uppercase text-white drop-shadow-lg transition-all hover:bg-blue-600 focus:bg-blue-700 active:bg-blue-700"
     >
       {children}
     </a>
   );
-}
+};
 
 export default Button;
